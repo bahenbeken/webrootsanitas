638adb5098ed8aadacfd353d71275444359325f8
tupa.panjaitan <tupa.panjaitan@techno1-consulting.com>
2018-11-16 16:25:41 +0700

    new image link

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<title>index</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<body bgcolor="#d2cfcf" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<!-- ImageReady Slices (index.psd) -->
+<center>
+<table id="Table_01" width="800" height="600" border="0" cellpadding="0" cellspacing="0">
+	<tr>
+		<td colspan="5">
+			<img src="images/index_01.gif" width="800" height="136" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="images/index_02.gif" width="48" height="318" alt=""></td>
+		<td>
+			<a href="http://ptdeltagro.ptdeltagro.com/backend">
+			<img src="images/index_03.gif" width="322" height="318" alt="">
+			</a>
+		</td>
+		<td>
+			<img src="images/index_04.gif" width="57" height="318" alt=""></td>
+		<td>
+			<a href="http://ptdeltagro.ptdeltagro.com/distribusi">
+			<img src="images/index_05.gif" width="322" height="318" alt="">
+			</a>
+		</td>
+		<td>
+			<img src="images/index_06.gif" width="51" height="318" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="5">
+			<img src="images/index_07.gif" width="800" height="146" alt=""></td>
+	</tr>
+</table>
+</center>
+<!-- End ImageReady Slices -->
+</body>
+</html>
